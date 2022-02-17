e72016f33f1015a24c4eda51cc1d2c6b9e6aa5a3
Talaxy <siwei.chen@shanbay.com>
2022-02-17 17:27:48 +0800

    feat: tailwind doc & demo

--- src/pages/test/2022-02-15.tsx
+++ src/pages/test/2022-02-15.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import Layout from '@theme/Layout';
+import '@site/src/css/tailwind.css';
+import { icon, data, imageURL } from './constant';
 
 const View = () => {
     return (
-        <Layout pageClassName="tailwind">
-            <div className="text-shanbay">Tailwind CSS</div>
-        </Layout>
+        <div className="p-5 bg-[#000]">
+            <div className="flex flex-col m-[15px] w-[340px] rounded-[12px] bg-[#fff] divide-[#ececec] divide-y-[1px]">
+                {data.map((section) => (
+                    <div
+                        className="flex items-center p-[20px] pr-[15px]"
+                        key={section.title}
+                    >
+                        <img
+                            className="w-[30px] aspect-square mr-[10px]"
+                            src={imageURL}
+                        />
+                        <div className="grow font-bold">{section.title}</div>
+                        <div className="w-[18px] aspect-square">
+                            {icon(section.done ? '#28d3b0' : '#eee')}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 
