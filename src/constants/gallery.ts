e1c9372a6039c5ca15304b5a357b82058db6aa80
Talaxy <fujianchensiwei@163.com>
2022-06-22 19:10:40 +0800

    feat: new post

--- src/constants/gallery.ts
+++ src/constants/gallery.ts
@@ -7,16 +7,28 @@ export interface GalleryCard {
 
 export const GALLERY_CARDS: Array<GalleryCard> = [
     {
+        title: '青岛 八大关',
+        subtitle: '2022年6月19日',
+        bg: 'http://r.photo.store.qq.com/psc?/V53zNsw50AU6SY3IaO3s4AEy7E1YXgc2/bqQfVz5yrrGYSXMvKr.cqWDKRrd.3QxGgfW2KijRH5r3dfgHoGrZWTo.SZZmMP9ojFtXTvjQpWtMnSdwTxfDo4q3HVc773c5jKsPwudVYbs!/r',
+        link: '/gallery/2022/2022-06-19',
+    },
+    {
+        title: '青岛 鲁迅公园',
+        subtitle: '2022年6月18日',
+        bg: 'http://r.photo.store.qq.com/psc?/V53zNsw50AU6SY3IaO3s4AEy7E1YXgc2/bqQfVz5yrrGYSXMvKr.cqUBCC9vjbdpLmul1qZ1vccKuwuQJKZ1YoT6ju77E2MnVK*qXuGHb*GLcgcl7ryuiuzj.a5cY8Dx2b6ENirwxky0!/r',
+        link: '/gallery/2022/2022-06-18',
+    },
+    {
+        title: '南京 莫愁湖公园',
+        subtitle: '2022年6月12日',
+        bg: 'http://r.photo.store.qq.com/psc?/V12to3FW3aSvFz/bqQfVz5yrrGYSXMvKr.cqeKnGXoSBhSFsVaoDq4HIwSrURMBvVjbHa2tq2WVIj.opMjmW4YaqzHRGwHYQ23wfs1bNfNqH941mjwyfo0CaRU!/r',
+        link: '/gallery/2022/2022-06-12',
+    },
+    {
         title: '南京 鱼嘴湿地公园',
         subtitle: '2022年4月5日',
         bg: 'http://r.photo.store.qq.com/psc?/V12to3FW3aSvFz/TmEUgtj9EK6.7V8ajmQrEDy3kOIK83DQjsj6aVxtW2uXMw549BSyKOROAUdd4N9MlgnFI9GLrnaf3V.IIejasW2xsOjTIb60I0n*2ioguQc!/r',
         link: '/gallery/2022/2022-04-05'
-    },
-    {
-        title: '数码宝贝',
-        subtitle: '2022年4月5日',
-        bg: 'http://r.photo.store.qq.com/psc?/V12to3FW21RiJP/TmEUgtj9EK6.7V8ajmQrELwpYRTe2bhr36RUr9vep6Ve5uOWseN1Vmjaxayr9ftWW1Cf.DQwZVPn0IEsLgImQLh47*g0vzmknUCd1IOtbyc!/r',
-        link: '/gallery/some/digimon'
     },
     {
         title: '南京 古秦淮',
