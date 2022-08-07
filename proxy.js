6a9a3df4b5595a862d48bc29894078f76d52129a
Talaxy <fujianchensiwei@163.com>
2022-08-07 12:44:14 +0800

    feat: proxy

---
+++ proxy.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req, res) {
+    proxy.web(req, res, { target: 'http://localhost:3000' });
+}).listen(80);
