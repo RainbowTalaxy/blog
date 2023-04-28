ea0d89f2b8d176fac1849ae794dad7c0b26dcd01
Talaxy <siwei.chen@shanbay.com>
2023-04-28 10:58:07 +0800

    feat: 样式调整

--- src/modules/word-bank/index.tsx
+++ src/modules/word-bank/index.tsx
@@ -1,11 +1,11 @@
-import Layout from '@theme/Layout';
 import styles from './index.module.css';
 import { useCallback, useEffect, useState } from 'react';
 import { Book, BookInfo, Word } from './type';
 import { AppleDate } from '@site/src/utils';
 import clsx from 'clsx';
 import QueryableWord from './components/QueryableWord';
-import QueryPanel from './components/QueryPanel';
+import useQuery from '@site/src/hooks/useQuery';
+import { useHistory } from '@docusaurus/router';
 
 function decoratePardOfSpeech(word: Word) {
     if (!word.part) return;
@@ -16,6 +16,10 @@ function decoratePardOfSpeech(word: Word) {
 const WordBank = () => {
     const [list, setList] = useState<BookInfo[]>([]);
     const [book, setBook] = useState<Book | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    // 帮我监听 query 参数，参数名为 id
+    const query = useQuery();
+    const history = useHistory();
 
     const refetchBook = useCallback(async (bookInfo: BookInfo) => {
         try {
@@ -24,95 +28,100 @@ const WordBank = () => {
             );
             const data = (await response.json()) as { book: Book };
             setBook(data.book);
+            setIsLoading(false);
         } catch {}
     }, []);
 
     const refetch = useCallback(async () => {
         try {
+            const id = query.get('id');
+            if (!id || book?.id.startsWith(id)) return;
+            setIsLoading(true);
             const response = await fetch(
                 'https://blog.talaxy.cn/public-api/word-bank/books/talaxy',
             );
             const data = (await response.json()) as { books: BookInfo[] };
             data.books.sort((a, b) => b.date - a.date);
             setList(data.books);
-            if (!book) {
-                refetchBook(data.books[0]);
-            }
+            const bookInfo = data.books.find((bookInfo) =>
+                bookInfo.id.startsWith(id),
+            );
+            if (bookInfo) refetchBook(bookInfo);
         } catch {}
-    }, [book]);
+    }, [book, query]);
 
     useEffect(() => {
         refetch();
-    }, []);
+    }, [query.get('id')]);
 
     return (
-        <Layout title="WordBank">
-            <div className={styles.container}>
-                <div className={styles.sidebar}>
-                    <div className={styles.sidebarInner}>
-                        <div className={styles.header}>WordBank</div>
-                        {list.map((bookInfo) => (
-                            <div
-                                className={clsx(
-                                    styles.book,
-                                    book?.id === bookInfo.id && styles.active,
-                                )}
-                                key={bookInfo.id}
-                                onClick={() => refetchBook(bookInfo)}
-                            >
-                                <div className={styles.bookTitle}>
-                                    {bookInfo.title}
+        <div className={styles.container}>
+            <div className={styles.sidebar}>
+                <div className={styles.sidebarInner}>
+                    <div className={styles.header}>WordBank</div>
+                    {list.map((bookInfo) => (
+                        <div
+                            className={clsx(
+                                styles.book,
+                                book?.id === bookInfo.id && styles.active,
+                            )}
+                            key={bookInfo.id}
+                            onClick={() => {
+                                // 设置 query 来更新
+                                history.push('?id=' + bookInfo.id.slice(0, 5));
+                            }}
+                        >
+                            <div className={styles.bookTitle}>
+                                {bookInfo.title}
+                            </div>
+                            <div className={styles.spacer} />
+                            <div className={styles.bookDate}>
+                                {AppleDate(bookInfo.date).format('YYYY/MM/DD')}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.content}>
+                {/* 下面的组件是给小屏写的，用来代替上面的sidebar */}
+                <div className={styles.sidebarMobile}>
+                    {list.map((bookInfo) => (
+                        <div
+                            className={clsx(
+                                styles.book,
+                                book?.id === bookInfo.id && styles.active,
+                            )}
+                            key={bookInfo.id}
+                            onClick={() => refetchBook(bookInfo)}
+                        >
+                            <div className={styles.bookTitle}>
+                                {bookInfo.title}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {book && (
+                    <div className={styles.wordList}>
+                        {book.words?.map((word) => (
+                            <div className={styles.word} key={word.id}>
+                                <div className={styles.wordName}>
+                                    {!isLoading && (
+                                        <QueryableWord word={word} />
+                                    )}
                                 </div>
                                 <div className={styles.spacer} />
-                                <div className={styles.bookDate}>
-                                    {AppleDate(bookInfo.date).format(
-                                        'YYYY/MM/DD',
-                                    )}
+                                <div className={styles.wordPartOfSpeech}>
+                                    {!isLoading && decoratePardOfSpeech(word)}
+                                </div>
+                                <div className={styles.wordDefinition}>
+                                    {!isLoading && word.def}
                                 </div>
                             </div>
                         ))}
                     </div>
-                </div>
-                {book && (
-                    <div className={styles.content}>
-                        {/* 下面的组件是给小屏写的，用来代替上面的sidebar */}
-                        <div className={styles.sidebarMobile}>
-                            {list.map((bookInfo) => (
-                                <div
-                                    className={clsx(
-                                        styles.book,
-                                        book?.id === bookInfo.id &&
-                                            styles.active,
-                                    )}
-                                    key={bookInfo.id}
-                                    onClick={() => refetchBook(bookInfo)}
-                                >
-                                    <div className={styles.bookTitle}>
-                                        {bookInfo.title}
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        <div className={styles.wordList}>
-                            {book.words?.map((word) => (
-                                <div className={styles.word} key={word.id}>
-                                    <div className={styles.wordName}>
-                                        <QueryableWord word={word} />
-                                    </div>
-                                    <div className={styles.spacer} />
-                                    <div className={styles.wordPartOfSpeech}>
-                                        {decoratePardOfSpeech(word)}
-                                    </div>
-                                    <div className={styles.wordDefinition}>
-                                        {word.def}
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
                 )}
             </div>
-        </Layout>
+        </div>
     );
 };
 
--- src/pages/word-bank.tsx
+++ src/pages/word-bank.tsx
@@ -1,7 +1,20 @@
 import WordBank from '@site/src/modules/word-bank';
+import Layout from '@theme/Layout';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    footer {
+        display: none;
+    }
+`;
 
 const Page = () => {
-    return <WordBank />;
+    return (
+        <Layout title="WordBank">
+            <GlobalStyle />
+            <WordBank />
+        </Layout>
+    );
 };
 
 export default Page;
