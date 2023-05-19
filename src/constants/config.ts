e8e7734f0e3cd3088534104aff16f8c400c9fba3
RainbowTalaxy <fujianchensiwei@163.com>
2023-05-19 10:48:40 +0800

    chore: 删除多余的文件

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,9 +1,8 @@
-const SERVER_URL = 'https://blog.talaxy.cn';
+export const SERVER_URL = 'https://blog.talaxy.cn';
+export const LOCAL_API = 'http://localhost:4000';
 
 export const SERVER_API = `${SERVER_URL}/public-api`;
 export const STATICS_URL = `${SERVER_URL}/statics`;
 
-export const LOCAL_API = 'http://localhost:4000';
-
-export const AZALEA_URL = 'https://blog.talaxy.cn/azalea';
 export const AZALEA_LOCAL_URL = 'http://127.0.0.1:5000';
+export const AZALEA_URL = `${SERVER_URL}/azalea`;
