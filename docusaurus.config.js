fcf6a7d5d80b4a8f97cdeee42e725ecbe9d0fab7
RainbowTalaxy <fujianchensiwei@163.com>
2023-07-14 14:11:03 +0800

    feat: 加入 mermaid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,6 +17,11 @@ const config = {
     organizationName: 'RainbowTalaxy', // Usually your GitHub org/user name.
     projectName: 'blog', // Usually your repo name.
 
+    markdown: {
+        mermaid: true,
+    },
+    themes: ['@docusaurus/theme-mermaid'],
+
     plugins: [
         () => ({
             name: 'tailwind-extension',
