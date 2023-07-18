5e6316eab594b118a0742f7362b4fcefe06d929b
RainbowTalaxy <fujianchensiwei@163.com>
2023-07-18 14:41:35 +0800

    fix: 用户设置跳转

--- src/hooks/useUserEntry.ts
+++ src/hooks/useUserEntry.ts
@@ -1,19 +1,26 @@
+import { useHistory } from '@docusaurus/router';
 import { useEffect } from 'react';
 import { getUser } from '../utils/user';
 
 const useUserEntry = () => {
+    const history = useHistory();
+
     useEffect(() => {
         const user = getUser();
         const element = document.querySelector<HTMLAnchorElement>(
             '.header-placeholder',
         );
         element.innerHTML = user.id || '设置用户';
-        element.setAttribute(
-            'href',
-            '/user' + '?nextUrl=' + encodeURIComponent(window.location.href),
-        );
         element.removeAttribute('target');
         element.removeAttribute('rel');
+        element.addEventListener('click', (e) => {
+            e.preventDefault();
+            history.push(
+                '/user' +
+                    '?nextUrl=' +
+                    encodeURIComponent(window.location.href),
+            );
+        });
         element.style.background = 'var(--ifm-menu-color-background-active)';
         element.style.borderRadius = '0.25rem';
         element.style.marginRight = '8px';
