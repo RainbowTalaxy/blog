875a7d62cc1639fa9831f7acd3196f4e01dad292
RainbowTalaxy <fujianchensiwei@163.com>
2023-12-01 09:53:35 +0800

    chore: 页面文件位置调整

---
+++ src/modules/zhaoyun/pages/index.tsx
@@ -0,0 +1,7 @@
+import Layout from '@theme/Layout';
+
+const Page = () => {
+    return <Layout title="赵云杯">赵云杯</Layout>;
+};
+
+export default Page;
---
+++ src/pages/zhaoyun/2022.tsx
@@ -0,0 +1,1 @@
+export { default } from '@site/src/modules/zhaoyun/pages/2022';
